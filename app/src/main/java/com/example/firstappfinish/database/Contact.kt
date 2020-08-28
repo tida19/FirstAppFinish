dfcf7aa2f31b3061b50c2c33b8f7af32bec8d9e2
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2020-08-28 14:38:37 +0700

    Annotation the Contact data class

--- app/src/main/java/com/example/firstappfinish/database/Contact.kt
+++ app/src/main/java/com/example/firstappfinish/database/Contact.kt
@@ -1,6 +1,15 @@
 package com.example.firstappfinish.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contact_table")
 data class Contact (
+    @PrimaryKey(autoGenerate = true)
     var id: Long = 0L,
+    @ColumnInfo(name = "name")
     var name: String = "unnamed",
+    @ColumnInfo(name = "phone")
     var phone: String = ""
 )
